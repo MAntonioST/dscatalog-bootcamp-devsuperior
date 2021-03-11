910fa8576345f43839b78213b7abde67de29dbc3
Marco Antonio <marco.teixeira4@fatec.sp.gov.br>
2021-03-11 19:23:34 -0300

    Add ProductRepositoryTests

---
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/repositories/ProductRepositoryTests.java
@@ -0,0 +1,134 @@
+package com.devsuperior.dscatalog.tests.repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.tests.factory.ProductFactory;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	private Long existingId;
+	private Long nonExistingId;
+	private Long countTotalProducts;
+	private Long countPCGamerProducts;
+	private PageRequest pageRequest;
+	private Long countTotalProductsCategory;
+	
+	@BeforeEach
+    void setUP()throws Exception {
+    	existingId = 1L;
+    	nonExistingId = 1000L;
+    	countTotalProducts = 25L;
+    	countPCGamerProducts = 21L;
+    	pageRequest = PageRequest.of(0, 10);
+    	countTotalProductsCategory = 23L;
+    }
+	
+	@Test
+	public void findShouldReturnAllProductsWhenCategoriesNotInformed() {
+		
+		List<Category> categories = null;
+		
+		
+		Page<Product> result = repository.find(categories, "", pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+	}
+	
+	@Test
+	public void findShouldReturnProductsWhenCategoriesInformed() {
+		
+		List<Category> categories = new ArrayList<>();
+		categories.add(new Category(3L, null));
+		
+		Page<Product> result = repository.find(categories, "", pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(countTotalProductsCategory, result.getTotalElements());
+	}
+	
+	
+	@Test
+	public void findShouldReturnAllProductsWhenNameIsEmpty() {
+		
+		String name = "";
+		
+		Page<Product> result = repository.find(null, name, pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+	}
+	
+	@Test
+	public void findShouldReturnProductsWhenNameExistsIgnoreCase() {
+		
+		String name = "pc gAMer";
+		
+		Page<Product> result = repository.find(null, name, pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(countPCGamerProducts, result.getTotalElements());
+	}
+	
+	@Test
+	public void findShouldReturnProductsWhenNameExists() {
+		
+		String name = "PC GAME";
+		
+		Page<Product> result = repository.find(null, name, pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(countPCGamerProducts, result.getTotalElements());
+	}
+	
+	@Test
+	public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+		
+		Product product = ProductFactory.createProduct();
+		product.setId(null);
+		
+		product = repository.save(product);
+		Optional<Product> result = repository.findById(product.getId());
+		
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts + 1, product.getId());
+		Assertions.assertTrue(result.isPresent());
+		Assertions.assertSame(result.get(), product);
+	}
+	
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExist(){
+		
+		repository.deleteById(existingId);
+		
+		Optional<Product> result = repository.findById(existingId);
+		
+		Assertions.assertFalse(result.isPresent());
+	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionIdDoesNotExist(){
+		
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			repository.deleteById(nonExistingId);
+		});
+		
+	}
+}
