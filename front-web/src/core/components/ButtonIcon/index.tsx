e5c6327c22979e99f774218716ba4a984bb100de
Marco Antonio <marco.teixeira4@fatec.sp.gov.br>
2021-05-04 09:04:56 -0300

    Create auth module

--- front-web/src/core/components/ButtonIcon/index.tsx
+++ front-web/src/core/components/ButtonIcon/index.tsx
@@ -3,20 +3,18 @@ import { ReactComponent as ArrowIcon } from 'core/assets/images/arrow.svg';
 import './styles.scss';
 
 type Props = {
-    text: string;
+  text: string;
 }
 
 const ButtonIcon = ({ text }: Props) => (
   <div className="d-flex">
-      <button className="btn btn-primary btn-icon">
-        <h5>{text}</h5>
-      </button>  
-      <div className="btn-icon-content">
-        <ArrowIcon />
-      </div>
+    <button className="btn btn-primary btn-icon">
+      <h5>{text}</h5>
+    </button>
+    <div className="btn-icon-content">
+      <ArrowIcon data-testid="arrow-icon" />
+    </div>
   </div>
-
-
 );
 
 export default ButtonIcon;
