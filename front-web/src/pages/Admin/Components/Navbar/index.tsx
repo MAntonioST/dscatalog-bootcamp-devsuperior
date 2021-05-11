3869ea48df61cf997ae2cdf7c8b4c0bca39e6c79
Marco Antonio <marco.teixeira4@fatec.sp.gov.br>
2021-05-11 08:06:01 -0300

    Add allowed roles on components

--- front-web/src/pages/Admin/Components/Navbar/index.tsx
+++ front-web/src/pages/Admin/Components/Navbar/index.tsx
@@ -1,20 +1,29 @@
+import { isAllowedByRole } from 'core/utilis/auth';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './styles.scss';
 
 const Navbar = () => (
     <nav className="admin-nav-container">
-      <ul>
-          <li>
-              <NavLink to="/admin/products" className="admin-nav-item">Meus Produtos</NavLink>
-          </li>
-          <li>
-              <NavLink to="/admin/categories" className="admin-nav-item">Minhas Categorias</NavLink>
-          </li>
-          <li>
-              <NavLink to="/admin/users" className="admin-nav-item">Meus Usuários</NavLink>
-          </li>
-      </ul>
+        <ul>
+            <li>
+                <NavLink to="/admin/products" className="admin-nav-item">
+                    Meus Produtos
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/admin/categories" className="admin-nav-item">
+                    Minhas Categorias
+                </NavLink>
+            </li>
+            {isAllowedByRole(['ROLE_ADMIN']) && (
+            <li>
+                 <NavLink to="/admin/users" className="admin-nav-item">
+                     Meus Usuários
+                 </NavLink>
+            </li>
+            )}
+        </ul>
     </nav>
 );
 
